0a71f845495a9acf49daaed9930d5fb6292b4631
audreyjy <audrey.youngberg1@gmail.com>
2022-04-04 12:00:47 -0600

    Add project files.

---
+++ UtahCollisions/Models/CollisionsDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UtahCollisions.Models
+{
+    public class CollisionsDBContext : DbContext 
+    {
+        public CollisionsDBContext(DbContextOptions<CollisionsDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Collision> TableNameInDataBase { get; set; }
+    }
+}
