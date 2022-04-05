b247d81408266b6e0f8ba643f238f662d11e4454
audreyjy <audrey.youngberg1@gmail.com>
2022-04-05 09:40:48 -0600

    filter by severity dropdown

--- UtahCollisions/Controllers/HomeController.cs
+++ UtahCollisions/Controllers/HomeController.cs
@@ -29,13 +29,15 @@ namespace UtahCollisions.Controllers
             return View();
         }
 
-        public IActionResult SummaryData(int pageNum = 1)
+        public IActionResult SummaryData(string severityID, int pageNum = 1)
         {
             int pageSize = 100;
 
             var x = new CollisionsViewModel
             {
+
                 Utah_Crash_Data_2020 = repo.Utah_Crash_Data_2020
+                .Where(x => x.CRASH_SEVERITY_ID.ToString() == severityID)
                 .Skip((pageNum - 1) * pageSize)
                 .Take(pageSize),
 
@@ -47,6 +49,8 @@ namespace UtahCollisions.Controllers
                 }
 
             }; 
+
+
 
             return View(x); 
         }
