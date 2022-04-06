fd46b197e6c5c6976a7c838b716e55deaffbad8b
markdvogt <90359765+markdvogt@users.noreply.github.com>
2022-04-06 15:03:43 -0600

    Finished ONNX frame

--- UtahCollisions/Models/CrashMLData.cs
+++ UtahCollisions/Models/CrashMLData.cs
@@ -9,26 +9,22 @@ namespace UtahCollisions.Models
     public class CrashMLData
     {
         //Insert features user imputs to get prediction here//
-        //MAKE SURE THEY ARE ALL FLOATS//
         /////////////////////////////////////////////////////
 
-        //public float MedianIncome { get; set; }
-        //public float MedianHouseAge { get; set; }
-        //public float AverageNumberOfRooms { get; set; }
-        //public float AverageNumberOfBedrooms { get; set; }
-        //public float Population { get; set; }
-        //public float AverageOccupancy { get; set; }
-        //public float Latitude { get; set; }
-        //public float Longitude { get; set; }
-
+        public float MilePoint { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public float Day { get; set; }
+        public float Month { get; set; }
+        public float Hour { get; set; }
+        
         public Tensor<float> AsTensor()
         {
             float[] data = new float[]
             {
-            //MedianIncome, MedianHouseAge, AverageNumberOfRooms, AverageNumberOfBedrooms,
-            //Population, AverageOccupancy, Latitude, Longitude
+                MilePoint, Latitude, Longitude, Day, Month, Hour
             };
-            int[] dimensions = new int[] { 1, 8 }; //I think the 8 represents all the features
+            int[] dimensions = new int[] { 1, 6 };
             return new DenseTensor<float>(data, dimensions);
         }
     }
